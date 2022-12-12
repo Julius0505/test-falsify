bd7c352e5eedf72a84338e47f0bdebf0ba957609
julius0505 <julius9605@gmail.com>
2022-12-11 23:48:40 -0500

    init

---
+++ src/falsify.js
@@ -0,0 +1,9 @@
+export default function falsify(func) {
+  // should return a function that returns the opposite boolean value
+  // of the given `func`
+  // see falsify.test.js for test cases
+  return function (...args) {
+    const f1 = func.bind(this);
+    return !f1(...args);
+  };
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+// please see falsify.js
+
+// you can run the test suite by click "show tests"
+// at the top right of the screen.
